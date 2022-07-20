a57163eaa8858b9de0ec1b64daa71b27c4f58450
Valeri Zanguri <zanguraa@gmail.com>
2022-07-21 01:17:11 +0400

    now program is fixed and not working while numbers are negative

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ let errorClass = document.getElementById("error");
 let border = document.getElementById("number-people")
 const inputMoney = document.getElementById("input-bill");
 const numberPeople = document.getElementById("number-people");
+const tipAmount = document.getElementsByClassName("first")[0];
+const totalAmount = document.getElementsByClassName("second")[0];
+const resetButton = document.getElementById("reset-button");
+let customInput = document.getElementById("custom");
 
-
+tipAmount.textContent = "$0.00";
+totalAmount.textContent = "$0.00";
 inputMoney.value = 0;
+numberPeople.value = 0;
+
 inputMoney.addEventListener("input", (e)=> {
     // console.log(e.target.value);
     
     
 });
 
-numberPeople.value = 0;
 numberPeople.addEventListener("input", (e)=> {
     // console.log(e.target.value);
     if(numberPeople.value > 0 ) {
@@ -34,44 +40,22 @@ function moneyOfPeople(percent) {
 peopleZero();
 }
 
-
-
-
-
-
-    function customPercent(customPer) {
+function customPercent(customPer) {
         if (numberPeople.value > 0 && inputMoney.value > 0) {
             const percentCheck = customPer.value / 100;
             const tip = inputMoney.value * percentCheck;
             const totalBill = Number(inputMoney.value) + tip;
 
-    
    tipAmount.innerHTML = ("$" + (tip / numberPeople.value).toFixed(2));
      totalAmount.innerHTML = ("$" + (totalBill / numberPeople.value).toFixed(2));
     }
     peopleZero();
  }
 
-
-
-// red border and text if number of people is equal 0
-
-
-
-
-// const customPercent = document.getElementById("custom");
-// customPercent.value = 0;
-// customPercent.addEventListener("input", (e)=> {
-    
-//     let cusTom = moneyOfPeople((parseFloat(customPercent.value)));
-//     tipAmount.innerHTML = ("$" + forPeople);
-//     totalAmount.innerHTML = ("$" + cusTom);
-    
-// });
-
-let checker = true;
+    let checker = true;
+// this function is for red border and text if number of people is equal 0
 function peopleZero() {
-    if (numberPeople.value == 0) {
+    if (numberPeople.value <= 0) {
     errorClass.style.display = "block";
     border.style.border = "2px solid red";
     checker = false
@@ -82,25 +66,18 @@ function peopleZero() {
     
 }
 
-
-
-const tipAmount = document.getElementsByClassName("first")[0];
-tipAmount.textContent = "$0.00";
-
-const totalAmount = document.getElementsByClassName("second")[0];
-totalAmount.textContent = "$0.00";
-
-const resetButton = document.getElementById("reset-button");
-
+    
+// this function will reset to default value
 function resetAll(resetButton) {
-tipAmount.textContent = "$0.00";
+    tipAmount.textContent = "$0.00";
 
 
-totalAmount.textContent = "$0.00";
+    totalAmount.textContent = "$0.00";
 
-inputMoney.value = 0;
-numberPeople.value = 0;
-customPercent.value = 0;
+    inputMoney.value = 0;
+    numberPeople.value = 0;
+    // customPercent.value = 0;
+    customInput.value = "Custom";
 }
 
 
